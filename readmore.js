658b7b4da0527f9a0fbfbabf351a87dcdc87a6cf
Szász Hanna Rebeka <szasz.hanna.rebeka@gmail.com>
2024-04-13 14:10:14 +0200

    adding readmore.js

---
+++ readmore.js
@@ -0,0 +1,26 @@
+const readmorebutton=document.getElementById("readmorebutton");
+const readmorediv=document.getElementById("readmorediv");
+const readmorep=document.getElementById("readmorep");
+
+readmorebutton.addEventListener("click", function(){
+    var request=new XMLHttpRequest();
+    request.open('GET', 'https://szaszhannarebeka.github.io/PracticeProjects-HTML-CSS-JavaScript/books.json');
+    request.onload=function() {
+        var data=JSON.parse(request.responseText);
+        renderHTML(data);
+    };
+    request.send();
+    readmorebutton.style.display="none";
+    readmorep.style.display="block";
+});
+
+function renderHTML(data){
+    var htmlString="";
+
+    for (i=0;i<data.length; i++){
+        htmlString += "<p>" + data[i].title + " by " + data[i].author + "</p>";
+    }
+
+    readmorediv.insertAdjacentHTML('beforeend', htmlString);
+}
+
